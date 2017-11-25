c3762034206a3d14d8590def8f5caa84a8b5721c
Andrei Cristian Petcu <andrei@ceata.org>
2017-11-25 21:49:47 +0200

    added good suggestions

--- background.js
+++ background.js
@@ -2,32 +2,33 @@ browser.omnibox.setDefaultSuggestion({
   description: `Search for containers and switch to them (e.g. "co personal" or "co bancking")`
 });
 
-// Update the suggestions whenever the input is changed.
 browser.omnibox.onInputChanged.addListener(async (text, addSuggestions) => {
   const contexts = await browser.contextualIdentities.query({});
+  var result = [];
   for (let context of contexts) {
     if (context.name.toLowerCase().indexOf(text.toLowerCase()) > -1) {
-      addSuggestions({
+      result.push({
         content: context.name,
         description: `Switch to container: ${context.name}`
-      });
+      })
     }
   }
+  addSuggestions(result);
 });
 
-// Open the page based on how the user clicks on a suggestion.
 browser.omnibox.onInputEntered.addListener(async (text, disposition) => {
   const contexts = await browser.contextualIdentities.query({});
   const tabs = await browser.tabs.query({ currentWindow: true, active: true });
   for (let context of contexts) {
     if (context.name.toLowerCase().indexOf(text.toLowerCase()) > -1) {
+      browser.tabs.remove(tabs[0].id);
       browser.tabs.create(
         {
           url: tabs[0].url,
-          cookieStoreId: context.cookieStoreId
+          cookieStoreId: context.cookieStoreId,
+          index: tabs[0].index
         }
       )
-      browser.tabs.remove({ id: tabs[0].id });
       break;
     }
   }
