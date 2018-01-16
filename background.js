1d2f92583857278d0e6660d12d253af74495410b
Matthew Potts <matt@viking>
2018-01-16 21:08:29 +0000

    Fix typos and code style

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 browser.omnibox.setDefaultSuggestion({
-  description: `Search for containers and switch to them (e.g. "co personal" or "co bancking")`
+  description: `Search for containers and switch to them (e.g. "co personal" or "co banking")`
 });
 
 browser.omnibox.onInputChanged.addListener(async (text, addSuggestions) => {
@@ -25,18 +25,19 @@ browser.omnibox.onInputEntered.addListener(async (text, disposition) => {
   contexts.push({
     cookieStoreId: 'firefox-default',
     name: 'default'
-  })
+  });
+
   for (let context of contexts) {
     if (context.name.toLowerCase().indexOf(text.toLowerCase()) > -1) {
       let tabCreateProperties = {
         cookieStoreId: context.cookieStoreId,
         index: tabs[0].index
-      }
-      if(tabs[0].url != 'about:newtab'){
+      };
+      if(tabs[0].url !== 'about:newtab') {
         tabCreateProperties.url = tabs[0].url;
       }
-      browser.tabs.create(tabCreateProperties)
-      browser.tabs.remove(tabs[0].id)
+      browser.tabs.create(tabCreateProperties);
+      browser.tabs.remove(tabs[0].id);
       break
     }
   }
