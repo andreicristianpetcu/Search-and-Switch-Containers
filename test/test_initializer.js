fb4c0898e303343bbddbd5376421a8adc5f70d07
Andrei Cristian Petcu <andrei@ceata.org>
2018-07-05 01:42:54 +0300

    finally some unit tests that actually do some work

--- test/test_initializer.js
+++ test/test_initializer.js
@@ -7,4 +7,8 @@ global.browser = browser;
 window.chrome = chrome;
 window.browser = browser;
 
+browser.omnibox.setDefaultSuggestion = sinon.spy();
+browser.omnibox.onInputChanged.addListener = sinon.spy();
+browser.contextualIdentities = {};
+
 console.log("initializing tests");
